22b9995293bb4c56274da8a95004e1f061c24683
alondracarrillocelis <alondra_3127200055@utd.edu.mx>
2025-07-07 21:29:06 -0600

    c

--- profesores/vite.config.js
+++ profesores/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/front_professors/', // 👈 importante para que funcionen rutas y assets
 })
